d7f37e93a3def1b4a28354527c0745a9d8a2880f
Aakash Thakur <mail.aakashthakur@gmail.com>
2021-03-14 17:34:47 +0545

    added few exercises

---
+++ src/com/f1softtraining/collectionexercisejava/map/ExerciseDriver.java
@@ -0,0 +1,17 @@
+package com.f1softtraining.collectionexercisejava.map;
+
+public class ExerciseDriver {
+    public static void main(String[] args) {
+        StudentsHashMap studentsHashMap = new StudentsHashMap();
+        studentsHashMap.students.put(1, "Aakash");
+        studentsHashMap.students.put(2, "Jikesh");
+        studentsHashMap.students.put(3, "Sabrin");
+        studentsHashMap.students.put(4, "Sandhya");
+        studentsHashMap.students.put(5, "Rashim");
+
+        System.out.println(studentsHashMap.students);
+
+        studentsHashMap.students.put(5, "Lucky");
+        System.out.println(studentsHashMap.students);
+    }
+}
---
+++ src/com/f1softtraining/collectionexercisejava/map/StudentsHashMap.java
@@ -0,0 +1,8 @@
+package com.f1softtraining.collectionexercisejava.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentsHashMap {
+    Map<Integer, String> students = new HashMap<>();
+}
